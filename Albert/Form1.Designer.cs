73172cd0ee059694f46e9b45d5e77d22d29df61f
Dmitrij Rusakov <Daemon2017@gmail.com>
2016-03-30 23:56:42 +0300

    1st

---
+++ Albert/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Albert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tower = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tower)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(797, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Огонь!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(797, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Угол +";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(797, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Угол -";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "20";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(797, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Мощность +";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(797, 145);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Мощность -";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Координата X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Координата Y";
+            // 
+            // tower
+            // 
+            this.tower.Image = global::Albert.Properties.Resources.tower;
+            this.tower.Location = new System.Drawing.Point(330, 140);
+            this.tower.Name = "tower";
+            this.tower.Size = new System.Drawing.Size(40, 70);
+            this.tower.TabIndex = 9;
+            this.tower.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Угол";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(201, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Скорость (м/с)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(392, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Время";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(458, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(392, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Осталось";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(458, 26);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "5";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(533, 7);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(59, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Выстрелы";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(599, 6);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(13, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 211);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tower);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 250);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(900, 250);
+            this.Name = "Form1";
+            this.Text = " Бомбарда";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.tower)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox tower;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+
+    }
+}
+
---
+++ Albert/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Albert
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        double x = 0, y = 0;
+        double h = 20;
+        double t = 0;
+        double V = 10;
+        const double g = 9.8154;
+        bool left, right, up, down, fire;
+        int c, z;
+        double d, f, l, u;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Program_Paint);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Enabled = false;
+            timer.Interval = 50;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+                t += 0.01;
+                x = V * t * Math.Cos(h * 3.14 / 180);
+                y = V * t * Math.Sin(h * 3.14 / 180) - (g * t * t) / 2;
+
+                f = x * 50;
+                label3.Text = f.ToString();
+                l = y * 50;
+                label4.Text = l.ToString();
+
+                d = t * 10;
+                label8.Text = d.ToString();
+
+                z = 5-c;
+                label10.Text = z.ToString();
+
+                label12.Text = u.ToString();
+
+                Invalidate();
+
+                if (l > 0 && l < 65 && f > tower.Location.X && f < (tower.Location.X + 40))
+                {
+                    timer.Stop();                    
+                    Point n = tower.Location;
+                    n.X += 100;
+                    tower.Location = n;
+                    c++;
+                    x = 0;
+                    y = 0;
+
+                    if (c==5)
+                    {
+                        MessageBox.Show("Конец игры", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        Application.Restart();
+                    }
+                }
+
+                if (y <= 0 | x >= 17.5)
+                {
+                    timer.Enabled = false;
+                }
+        }
+
+        void Program_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Red, new Rectangle((int)(50 * x), (int)(200 - 50 * y), 5, 5));           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = 0;
+            y = 0;
+            t = 0;
+            u++;
+            timer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (h <= 90)
+            {
+                h = h + 1;
+            }
+            else
+            {
+                h = h;
+            }
+            label1.Text = h.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (h >= 0)
+            {
+                h = h - 1;
+            }
+            else
+            {
+                h = h;
+            } 
+            label1.Text = h.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (V <= 15)
+            {
+                V = V + 1;
+            }
+            else
+            {
+                V = V;
+            }
+            label2.Text = V.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (V >= 0)
+            {
+                V = V - 1;
+            }
+            else
+            {
+                V = V;
+            }
+            label2.Text = V.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (timer.Enabled == false)
+            {
+                switch (e.KeyValue)
+                {
+                    case 'W':
+                        {
+                            up = true;
+                            button2_Click(sender, e);
+                            break;
+                        }
+                    case 'A':
+                        {
+                            left = true;
+                            button5_Click(sender, e);
+                            break;
+                        }
+                    case 'D':
+                        {
+                            right = true;
+                            button4_Click(sender, e);
+                            break;
+                        }
+                    case 'S':
+                        {
+                            down = true;
+                            button3_Click(sender, e);
+                            break;
+                        }
+                    case 32:
+                        {
+                            fire = true;
+                            button1_Click(sender, e);
+                            break;
+                        }
+                }
+            }
+        }
+        }
+    }
+
